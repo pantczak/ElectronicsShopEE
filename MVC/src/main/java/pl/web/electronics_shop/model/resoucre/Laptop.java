fd06aca5c6b33da83963f79272757c8623d720b1
pantczak <piotrmateuszantczak@protonmail.com>
2021-02-09 10:43:17 +0100

    Added resource bean validation

--- MVC/src/main/java/pl/web/electronics_shop/model/resoucre/Laptop.java
+++ MVC/src/main/java/pl/web/electronics_shop/model/resoucre/Laptop.java
@@ -3,17 +3,20 @@ package pl.web.electronics_shop.model.resoucre;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Laptop extends Resource{
+public class Laptop extends Resource {
+    @NotNull
+    @Size(min = 1, max = 32)
     private int memoryInGb;
 
     public Laptop(String brand, String model, boolean isAvailable, int weightInGrams, int memoryInGb) {
         super(brand, model, isAvailable, weightInGrams);
         this.memoryInGb = memoryInGb;
     }
-
-
 
 
 }
--- MVC/src/main/java/pl/web/electronics_shop/model/resoucre/Smartphone.java
+++ MVC/src/main/java/pl/web/electronics_shop/model/resoucre/Smartphone.java
@@ -4,10 +4,15 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 public class Smartphone extends Resource {
+    @NotNull
+    @Size(min = 1, max = 36)
     private double batteryLifetime;
 
     public Smartphone(String brand, String model, boolean isAvailable, int weightInGrams, double batteryLifetime) {
