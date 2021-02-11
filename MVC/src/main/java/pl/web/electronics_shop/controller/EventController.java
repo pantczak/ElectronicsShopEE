72c2242671852ae39fe577480e53a17bab874b49
pantczak <piotrmateuszantczak@protonmail.com>
2021-02-11 16:43:49 +0100

    partially implemented EventContorller

---
+++ MVC/src/main/java/pl/web/electronics_shop/controller/EventController.java
@@ -0,0 +1,63 @@
+package pl.web.electronics_shop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import pl.web.electronics_shop.model.Event;
+import pl.web.electronics_shop.service.EventService;
+import pl.web.electronics_shop.service.ResourceService;
+import pl.web.electronics_shop.service.UserService;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Named
+@SessionScoped
+public class EventController implements Serializable {
+
+    @Inject
+    @Getter
+    private UserService userService;
+
+    @Inject
+    @Getter
+    private ResourceService resourceService;
+
+    @Inject
+    @Getter
+    private EventService eventService;
+
+    @Getter
+    @Setter
+    private Event currentEvent;
+
+    @Getter
+    @Setter
+    private List<Event> currentEvents;
+
+    @Getter
+    @Setter
+    private Date borrowDate;
+
+    @Getter
+    @Setter
+    private UUID newEventClientUuid;
+
+    @Getter
+    @Setter
+    private UUID newEventDeviceUuid;
+
+
+    @PostConstruct
+    public void initList() {
+        currentEvents = eventService.getAllEvents();
+    }
+
+
+
+}
